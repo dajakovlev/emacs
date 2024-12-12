469a423004aabcdc073936d523920a141120936b
D. A. Jakovlev <dm.a.iakovlev@gmail.com>
2024-12-13 00:56:32 +0500

    Add straight.el

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;;;; early-init.el --- Early initialization of GNU Emacs -*- lexical-binding: t -*-
+
+;; Disable package.el in favor of straight.el
+(setq package-enable-at-startup nil)
+
+(provide 'early-init)
+;;; early-init.el ends here
---
+++ init.el
@@ -0,0 +1,32 @@
+;;;; init.el --- My configuration for GNU Emacs -*- lexical-binding: t -*-
+
+;;;
+;;; Package management
+;;;
+
+;; straight.el
+
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;;; use-package
+(straight-use-package 'use-package)
+
+;; Configure use-package to use straight.el by default
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
